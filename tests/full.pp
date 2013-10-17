c6c5ee6e5b360d54cd34ddc62eb95194faf063f8
Lucas Bickel <hairmare@purplehaze.ch>
2013-10-18 00:07:56 +0200

    add epel repo on redhat

---
+++ tests/full.pp
@@ -0,0 +1,26 @@
+/**
+ * puppet site containing a default node that install the whole shebang
+ *
+ * This site.pp is used by vagrant so set up the local machine.
+ */
+node default {
+
+ case $::osfamily {
+   'RedHat': {
+     class { 'epel':
+       before => Class['syslogng']
+     }
+   },
+   default: {}
+ }
+
+  class {
+    'syslogng':
+      logpaths => {
+        'mod_php' => {}
+      };
+    'gravity':
+  }
+
+  Class['syslogng'] -> Class['gravity']
+}
