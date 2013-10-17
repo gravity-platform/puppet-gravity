76a01e659ef25b024ec7f0543ee79fa39e58ca08
Lucas Bickel <hairmare@purplehaze.ch>
2013-10-18 00:51:53 +0200

    add mor logpaths and prepare for yum path

--- tests/full.pp
+++ tests/full.pp
@@ -5,20 +5,35 @@
  */
 node default {
 
- case $::osfamily {
-   'RedHat': {
-     class { 'epel':
-       before => Class['syslogng']
-     }
-   }
-   default: {}
- }
+  case $::osfamily {
+    'RedHat': {
+      class { 'epel':
+        before => Class['syslogng']
+      }
+
+      $distro_syslog_logpaths = {
+        # @todo create this in syslogng module
+        # 'yum' => {},
+      }
+    }
+    default: {
+      $distro_syslog_logpaths = {}
+    }
+  }
+
+  $syslog_logpaths_real = merge(
+    $distro_syslog_logpaths,
+    {
+      'syslog-ng' => {},
+      'sudo'      => {},
+      'sshd'      => {},
+      'mod_php'   => {},
+    }
+  )
 
   class {
     'syslogng':
-      logpaths => {
-        'mod_php' => {}
-      };
+      logpaths => $syslog_logpaths_real;
     'gravity':
   }
 
